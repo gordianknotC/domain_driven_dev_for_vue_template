76e6a95ea43c904798f7e7ecb703d45aaaba262a
curtis <curtis@fillygaming888.com>
2022-06-04 03:28:50 +0800

    Fix refine Wallet layout

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -8,7 +8,29 @@ container.page.page-main
     :showBack="true"
     @back="onClickBack"
   )
+  container.card-row
+    container.hover-card.card-paytm
+      img(:src="require('~/assets/mfi-paytm.png')")
+      span paytm
+    container.hover-card.card-amazon
+      img(:src="require('~/assets/mfi-amazon-pay.png')")
+      span amazon
+    container.hover-card.card-gpay
+      img(:src="require('~/assets/mfi-google-pay.png')")
+      span gpay
+  container.card-row
+    container.hover-card.card-ola
+      img(:src="require('~/assets/mfi-ola-money.png')")
+      span ola money
+    container.hover-card.card-jio
+      img(:src="require('~/assets/mfi-jio-money.png')")
+      span jio money
+    container.hover-card.card-free
+      img(:src="require('~/assets/mfi-freecharge.png')")
+      span freecharge
 
+  container.page-main-submit
+    button PAY ₹500.00
 
 </template>
 
@@ -68,7 +90,6 @@ export default defineComponent({
   }
   .subtitle{
     @apply text-left text-label;
-
   }
 
   @apply h-full relative;
@@ -79,23 +100,55 @@ export default defineComponent({
     // todo: remove this
     @apply px-3;
 
+    &-submit{
+      height: 4.4rem;
+      padding: 1rem 2.2rem;
+      margin-top: 0.8rem;
+      button{
+        @apply bg-primary text-white font-bold text-center items-center rounded w-full h-full;
+        &:hover{
+          @apply bg-primary-light;
+        }
+      }
+    }
+  }
+
+  //&-main::after{
+  //  content: "";
+  //  background-image: url("~@/assets/Main-EWallet.png");
+  //  background-size: contain;
+  //  background-repeat-x: no-repeat;
+  //  background-position-x: center;
+  //  opacity: 0.5;
+  //  top: -23px;
+  //  left: 0;
+  //  bottom: 0;
+  //  right: 0;
+  //  position: absolute;
+  //  z-index: -10;
+  //}
+
+
+  .card{
+    @apply flex-1 flex items-center h-full;
+    &-row{
+      @apply flex flex-row w-full;
+      height: 3.6rem;
+      margin-bottom: 0.2rem;
+      .hover-card{
+        @apply flex-1 flex items-center h-full;
+        img{
+          @apply h-full;
+        }
+        span{
+          @apply text-xs text-label font-Lexend;
+        }
+      }
+    }
 
   }
 
-  &-main::after{
-    content: "";
-    background-image: url("~@/assets/Main-EWallet.png");
-    background-size: contain;
-    background-repeat-x: no-repeat;
-    background-position-x: center;
-    opacity: 0.5;
-    top: -23px;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    position: absolute;
-    z-index: -10;
-  }
+
 
 
 }
