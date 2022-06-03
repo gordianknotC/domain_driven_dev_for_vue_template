1bd5bb69f08b05571c38ced22fbe37f5214d68da
curtis <curtis@fillygaming888.com>
2022-06-04 03:49:48 +0800

    Update package

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "vant/lib/index.css";
+// import "vant/lib/index.css";
 import "~/assets/styles/tailwind.scss";
-import {installVant, setupVToast} from "@/plugins/vantPlugin";
+// import {installVant, setupVToast} from "@/plugins/vantPlugin";
 import svgIconPlugin from "@/plugins/svgIcon";
 
 
 
 const app = createApp(App as any)
 // installVant(app);
-setupVToast(app);
+// setupVToast(app);
 app.use(svgIconPlugin, {imports: []});
 app.mount('#app');
--- src/plugins/vantPlugin.ts
+++ src/plugins/vantPlugin.ts
@@ -1,29 +1,29 @@
-import { App, Plugin } from "vue";
-import VToast, {PluginOptions, POSITION} from "vue-toastification";
-import {
-  Toast,
-  CountDown,
-}  from "vant";
-
-export const installVant: Exclude<Plugin["install"], undefined> = function installTemp(app: App) {
-  Object.entries({
-    CountDown,
-  }).forEach(([componentName, component]) => {
-    app.use(component as any);
-  });
-};
-
-export
-function setupVToast(app: App<Element>){
-  const vToastOpts: PluginOptions = {
-    position: POSITION.BOTTOM_CENTER,
-    hideProgressBar: true,
-    icon: false,
-    transition: "Vue-Toastification__fade",
-    maxToasts: 3
-  };
-  app.use(VToast, vToastOpts);
-}
-
-
-
+// import { App, Plugin } from "vue";
+// import VToast, {PluginOptions, POSITION} from "vue-toastification";
+// import {
+//   Toast,
+//   CountDown,
+// }  from "vant";
+//
+// export const installVant: Exclude<Plugin["install"], undefined> = function installTemp(app: App) {
+//   Object.entries({
+//     CountDown,
+//   }).forEach(([componentName, component]) => {
+//     app.use(component as any);
+//   });
+// };
+//
+// export
+// function setupVToast(app: App<Element>){
+//   const vToastOpts: PluginOptions = {
+//     position: POSITION.BOTTOM_CENTER,
+//     hideProgressBar: true,
+//     icon: false,
+//     transition: "Vue-Toastification__fade",
+//     maxToasts: 3
+//   };
+//   app.use(VToast, vToastOpts);
+// }
+//
+//
+//
