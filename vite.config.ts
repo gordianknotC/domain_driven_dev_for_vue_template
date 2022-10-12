f287a55635d5da7d651d26c88d7aa5a7ae4d102a
mickey <mickey@fillygaming888.com>
2022-10-12 17:19:38 +0800

    fix(vite):change-injectHtml-to-createHtmlPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,33 @@
 /// <reference types="vitest" />
 
-import { ConfigEnv, defineConfig, loadEnv, Plugin, UserConfig, UserConfigExport } from "vite";
-import path from 'node:path';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import ViteRequireContext from '@originjs/vite-plugin-require-context';
-import envCompatible from 'vite-plugin-env-compatible';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import {
+  ConfigEnv,
+  defineConfig,
+  loadEnv,
+  Plugin,
+  UserConfig,
+  UserConfigExport
+} from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import path from "node:path";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import ViteRequireContext from "@originjs/vite-plugin-require-context";
+import envCompatible from "vite-plugin-env-compatible";
+import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 // import pugPlugin from "vite-plugin-pug"
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
-const options = { pretty: true } // FIXME: pug pretty is deprecated!
-const locals = { name: "My Pug" }
+const options = { pretty: true }; // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" };
 
 export default ({ command, mode }: ConfigEnv) => {
   const root = process.cwd();
   const env = loadEnv(mode, root);
   const stringfiedEnv: Record<string, any> = {};
-  const isBuild = command === 'build';
+  const isBuild = command === "build";
 
-  Object.keys(env).forEach((key)=>{
+  Object.keys(env).forEach(key => {
     stringfiedEnv[key] = JSON.stringify(env[key]);
   });
 
@@ -28,43 +36,34 @@ export default ({ command, mode }: ConfigEnv) => {
 
   return defineConfig({
     root,
-    define:{
+    define: {
       ...stringfiedEnv,
-      global:{
-      }
+      global: {}
     },
     esbuild: {
-      target: 'esnext'
+      target: "esnext"
     },
     resolve: {
       alias: {
-        "@": path.resolve(root, 'src/'),
-        "~": path.resolve(root, 'src/'),
+        "@": path.resolve(root, "src/"),
+        "~": path.resolve(root, "src/")
       },
-      extensions: [
-        '.mjs',
-        '.js',
-        '.ts',
-        '.tsx',
-        '.json',
-        '.vue'
-      ]
+      extensions: [".mjs", ".js", ".ts", ".tsx", ".json", ".vue"]
     },
-    server: {
-    },
+    server: {},
     css: {
       preprocessorOptions: {
         //@ts-ignore
-        resolver(id, basedir, importOptions){
+        resolver(id, basedir, importOptions) {
           console.log("id, basedir, importopt", id, basedir, importOptions);
         },
-        scss:{
-          additionalData:`
+        scss: {
+          additionalData: `
             @import '@/presentation/assets/styles/predefined/mixin';
             @import '@/presentation/assets/styles/predefined/variables';
-          `,
+          `
         }
-      },
+      }
     },
     plugins: [
       vue(),
@@ -76,46 +75,15 @@ export default ({ command, mode }: ConfigEnv) => {
       // pugPlugin(options, locals),
       createSvgIconsPlugin({
         // Specify the icon folder to be cached
-        iconDirs: [
-          path.resolve(process.cwd(),
-            'presentation/assets/icons')
-        ],
+        iconDirs: [path.resolve(process.cwd(), "presentation/assets/icons")],
         // Specify symbolId format
-        symbolId: 'icon-[name]',
+        symbolId: "icon-[name]"
       }),
-      // injectHtml({
-      //   injectData: {
-      //     htmlWebpackPlugin: {
-      //       options: {
-      //         title: 'Fantasyee',
-      //         pwa: {
-      //           name: 'Fantasyee',
-      //           themeColor: '#1915bf',
-      //           msTileColor: '#000000',
-      //           appleMobileWebAppCapable: 'yes',
-      //           appleMobileWebAppStatusBarStyle: 'black',
-      //           workboxPluginMode: 'GenerateSW',
-      //           workboxOptions: {
-      //             skipWaiting: true,
-      //             clientsClaim: true,
-      //           },
-      //           iconPaths: {
-      //             favicon32: "img/icons/favicon-32x32.png",
-      //             favicon16: "img/icons/favicon-16x16.png",
-      //             appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
-      //             maskIcon: "img/icons/safari-pinned-tab.svg",
-      //             msTileImage: "img/icons/msapplication-icon-144x144"
-      //           },
-      //         }
-      //       }
-      //     }
-      //   }
-      // }),
+      createHtmlPlugin({
+        inject: {
+          data: { ...env, MODE: mode }
+        }
+      })
     ]
   });
-}
-
-
-
-
-
+};
