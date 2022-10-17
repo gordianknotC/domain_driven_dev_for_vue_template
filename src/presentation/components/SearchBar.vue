5a11c382432b9ae7417b4f0183cf499b3a2915a3
mickey <mickey@fillygaming888.com>
2022-10-17 09:55:41 +0800

    feat(SearchBar):defineComponent

---
+++ src/presentation/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-input v-model="input" :placeholder="placeholder" :prefix-icon="Search">
+    </el-input>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, watch } from "vue";
+import { Search } from "@element-plus/icons-vue";
+
+export default defineComponent({
+  name: "SearchBar",
+  props: {
+    placeholder: { type: String, default: "" }
+  },
+  emits: ["onValueChanged"],
+  setup(props, { emit }) {
+    const input = ref("");
+    watch(
+      () => input.value,
+      (val, prevVal) => {
+        if (val === prevVal) return;
+        emit("onValueChanged", val);
+      }
+    );
+    return {
+      props,
+      input,
+      Search
+    };
+  }
+});
+</script>
+<style lang="ts"></style>
