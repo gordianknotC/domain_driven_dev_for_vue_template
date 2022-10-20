5b975c675f800d4f7c505a520dd1d4346ea98d7e
curtis <curtis@fillygaming888.com>
2022-10-20 15:07:00 +0800

    Add demo for customized el-select

--- src/presentation/pages/demos/DemoIndexPage.vue
+++ src/presentation/pages/demos/DemoIndexPage.vue
@@ -1,19 +1,72 @@
 <template>
-  <h3>hello</h3>
+  <h3>customize el-select</h3>
+  <el-select
+    v-model="value"
+    size="large"
+    @focus="onFocus"
+    @click="onClick"
+    @show="onShow"
+    @hide="onHide"
+    @open="onOpen"
+    @close="onClose"
+  >
+    <template v-slot>
+      <section></section>
+    </template>
+    <template v-slot:empty>
+      <section>dropcontent: {{ dropContent }} :</section>
+    </template>
+  </el-select>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from "vue";
-export default defineComponent({
-  name: "DemoIndexPage",
-  components: {},
-  props: {},
-  emit: ["close", "route"],
-  setup(props, { emit }) {
-    /** 儲存參數 */
-    return {};
+<script lang="ts" setup>
+import { ref } from "vue";
+
+console.log("mount demo pageindex");
+const value = ref("");
+const dropContent = ref("");
+const onFocus = v => {
+  console.log("onFocus", v);
+  dropContent.value = v;
+};
+const onClick = () => {
+  console.log("click");
+};
+const onHide = () => {
+  console.log("hide");
+};
+
+const onShow = () => {
+  console.log("onShow");
+};
+const onOpen = () => {
+  console.log("onOpen");
+};
+const onClose = () => {
+  console.log("onClose");
+};
+const options = [
+  {
+    value: "Option1",
+    label: "Option1"
+  },
+  {
+    value: "Option2",
+    label: "Option2"
+  },
+  {
+    value: "Option3",
+    label: "Option3"
+  },
+  {
+    value: "Option4",
+    label: "Option4"
+  },
+  {
+    value: "Option5",
+    label: "Option5"
   }
-});
+];
 </script>
 
 <style lang="scss" scoped></style>
