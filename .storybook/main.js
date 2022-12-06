9b5fc51e71adeeadbc8f12d946036857be521893
curtis <curtis@fillygaming888.com>
2022-12-06 10:07:51 +0800

    Add storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,35 +1,35 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(mdx|js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-actions",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-actions"
+    "@storybook/addon-interactions"
   ],
   framework: "@storybook/vue3",
   core: {
     builder: "@storybook/builder-vite"
-  },
-  async viteFinal(config) {
-    return {
-      ...config,
-      esbuild: {
-        ...config.esbuild,
-        jsxInject: `import Vue from 'vue'`
-      },
-      rollupOptions: {
-        ...config.rollupOptions,
-        // Externalize deps that shouldn't be bundled
-        // external: ["react", "react-dom"],
-        output: {
-          // Global vars to use in UMD build for externalized deps
-          globals: {
-            //react: "React",
-            //"react-dom": "ReactDOM",
-          }
-        }
-      }
-    };
   }
+  // async viteFinal(config) {
+  //   return {
+  //     ...config,
+  //     esbuild: {
+  //       ...config.esbuild,
+  //       jsxInject: `import Vue from 'vue'`
+  //     },
+  //     rollupOptions: {
+  //       ...config.rollupOptions,
+  //       // Externalize deps that shouldn't be bundled
+  //       // external: ["react", "react-dom"],
+  //       output: {
+  //         // Global vars to use in UMD build for externalized deps
+  //         globals: {
+  //           //react: "React",
+  //           //"react-dom": "ReactDOM",
+  //         }
+  //       }
+  //     }
+  //   };
+  // }
 };
